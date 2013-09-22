d9ef2b44da7dd8f00fb338e8755444a2e4a9b063
Dmitry Korotkov <codename539@gmail.com>
2013-09-23 02:33:20 +0400

    using update java alternatives

--- support/ci-travis.sh
+++ support/ci-travis.sh
@@ -1,14 +1,11 @@
 echo "Switching to openjdk6"
-sudo jdk_switcher use openjdk6
+sudo update-java-alternatives -s java-1.6.0-openjdk-amd64
 echo $JAVA_HOME
 echo "Switching to openjdk7"
-sudo jdk_switcher use openjdk8
+sudo update-java-alternatives -s java-1.7.0-openjdk-amd64
 echo $JAVA_HOME
 echo "Switching to oracle-jdk7"
-sudo jdk_switcher use oracle-jdk7
+sudo update-java-alternatives -s java-7-oracle
 echo $JAVA_HOME
 
-
-update-java-alternatives -l
-
-exit 0
+exit "Testing build failed"

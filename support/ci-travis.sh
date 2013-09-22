12fb9ca82118f384cf22e8d7bb8458e5f3f86045
Dmitry Korotkov <codename539@gmail.com>
2013-09-23 03:33:14 +0400

    fanal change java_home

--- support/ci-travis.sh
+++ support/ci-travis.sh
@@ -1,10 +1,11 @@
 sudo apt-get -qq update
 mvn --version
 sudo apt-get -qq install openjdk-6-jdk openjdk-7-jdk icedtea-6-plugin icedtea-7-plugin maven
+sudo apt-get dist-upgrade
 mvn --version
 echo "Switching to openjdk6"
 sudo update-java-alternatives -s java-1.6.0-openjdk-amd64
-JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/java::")
+JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
 echo "JAVA_HOME is"
 echo $JAVA_HOME
 echo "java version is"
@@ -12,7 +13,7 @@ java -version
 javac -version
 echo "Switching to openjdk7"
 sudo update-java-alternatives -s java-1.7.0-openjdk-amd64
-JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/java::")
+JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
 echo "JAVA_HOME is"
 echo $JAVA_HOME
 echo "java version is"
@@ -21,11 +22,11 @@ javac -version
 echo "-----------------------"
 echo "Switching to oracle-jdk7"
 sudo update-java-alternatives -s java-7-oracle
-JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/java::")
+JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
 echo "JAVA_HOME is"
 echo $JAVA_HOME
 echo "java version is"
 java -version
 javac -version
 
-exit "Testing build failed"
+exit "0"

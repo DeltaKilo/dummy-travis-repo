27452c3278656df891757365a83d46fcd9429501
Dmitry Korotkov <codename539@gmail.com>
2013-09-23 02:18:16 +0400

    another one fix playing with jdk_switcher

---
+++ support/ci-travis.sh
@@ -0,0 +1,11 @@
+echo "Switching to openjdk6"
+sudo jdk_switcher use openjdk6
+echo $JAVA_HOME
+echo "Switching to openjdk7"
+sudo jdk_switcher use openjdk8
+echo $JAVA_HOME
+echo "Switching to oracle-jdk7"
+sudo jdk_switcher use oracle-jdk7
+echo $JAVA_HOME
+
+

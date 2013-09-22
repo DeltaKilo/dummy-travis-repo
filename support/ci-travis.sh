d14221e3dcb63b26648a8e55ffd64eba256e9a04
Dmitry Korotkov <codename539@gmail.com>
2013-09-23 03:44:48 +0400

    now we have 2 oracle java versions

--- support/ci-travis.sh
+++ support/ci-travis.sh
@@ -1,7 +1,7 @@
 sudo apt-get -qq update
-mvn --version
-sudo apt-get -qq install openjdk-6-jdk openjdk-7-jdk icedtea-6-plugin icedtea-7-plugin maven
-sudo apt-get dist-upgrade
+sudo apt-get -qq install openjdk-6-jdk openjdk-7-jdk icedtea-6-plugin icedtea-7-plugin
+sudo apt-mark hold oracle-java7-installer
+sudo apt-get -qq dist-upgrade
 mvn --version
 echo "Switching to openjdk6"
 sudo update-java-alternatives -s java-1.6.0-openjdk-amd64
@@ -20,7 +20,7 @@ echo "java version is"
 java -version
 javac -version
 echo "-----------------------"
-echo "Switching to oracle-jdk7"
+echo "Switching to oracle-jdk7u25"
 sudo update-java-alternatives -s java-7-oracle
 JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
 echo "JAVA_HOME is"
@@ -28,5 +28,18 @@ echo $JAVA_HOME
 echo "java version is"
 java -version
 javac -version
+echo "-----------------------"
+echo "Updating Oracle JDK to u40"
+sudo apt-mark hold oracle-java7-installer
+sudo apt-get -qq install oracle-java7-installer
+echo "Switching to oracle-jdk7u40"
+sudo update-java-alternatives -s java-7-oracle
+JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+echo "JAVA_HOME is"
+echo $JAVA_HOME
+echo "java version is"
+java -version
+javac -version
+echo "-----------------------"
 
 exit "0"

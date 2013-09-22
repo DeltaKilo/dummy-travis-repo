8654b1746c3d41bd61a0ed1491391b1718f080cc
Dmitry Korotkov <codename539@gmail.com>
2013-09-23 02:45:32 +0400

    change way to determine java

--- support/ci-travis.sh
+++ support/ci-travis.sh
@@ -3,17 +3,16 @@ sudo apt-get install icedtea-6-plugin icedtea-7-plugin
 echo "Switching to openjdk6"
 sudo update-java-alternatives -s java-1.6.0-openjdk-amd64
 echo $JAVA_HOME
-echo `which java`
-echo `which javac`
+java -version
 echo "-----------------------"
 echo "Switching to openjdk7"
 sudo update-java-alternatives -s java-1.7.0-openjdk-amd64
 echo $JAVA_HOME
-echo `which java`
-echo `which javac`
+java -version
 echo "-----------------------"
 echo "Switching to oracle-jdk7"
 sudo update-java-alternatives -s java-7-oracle
 echo $JAVA_HOME
+java -version
 
 exit "Testing build failed"

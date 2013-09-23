d8200228a7062de5a23730773720a3a92f983441
Dmitry Korotkov <codename539@gmail.com>
2013-09-23 04:53:35 +0400

    refactor something and add additonal info

--- support/ci-travis.sh
+++ support/ci-travis.sh
@@ -1,19 +1,30 @@
+#!/bin/sh
+
+function switch_jdk {
+ sudo update-java-alternatives -s $1
+ JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+ echo "Java switched to $1"
+}
+
+echo "*** Memory, CPU & kernel info ***"
+free -h
+lscpu
+uname -a
+
 sudo apt-get -qq update
 sudo apt-get -qq install openjdk-6-jdk openjdk-7-jdk icedtea-6-plugin icedtea-7-plugin
 sudo apt-mark hold oracle-java7-installer
 sudo apt-get -qq dist-upgrade
 mvn --version
 echo "Switching to openjdk6"
-sudo update-java-alternatives -s java-1.6.0-openjdk-amd64
-JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+switch_jdk java-1.6.0-openjdk-amd64
 echo "JAVA_HOME is"
 echo $JAVA_HOME
 echo "java version is"
 java -version
 javac -version
 echo "Switching to openjdk7"
-sudo update-java-alternatives -s java-1.7.0-openjdk-amd64
-JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+switch_jdk java-1.7.0-openjdk-amd64
 echo "JAVA_HOME is"
 echo $JAVA_HOME
 echo "java version is"
@@ -21,8 +32,7 @@ java -version
 javac -version
 echo "-----------------------"
 echo "Switching to oracle-jdk7u25"
-sudo update-java-alternatives -s java-7-oracle
-JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+switch_jdk java-7-oracle
 echo "JAVA_HOME is"
 echo $JAVA_HOME
 echo "java version is"
@@ -33,8 +43,7 @@ echo "Updating Oracle JDK to u40"
 sudo apt-mark unhold oracle-java7-installer
 sudo apt-get -qq install oracle-java7-installer
 echo "Switching to oracle-jdk7u40"
-sudo update-java-alternatives -s java-7-oracle
-JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+switch_jdk java-7-oracle
 echo "JAVA_HOME is"
 echo $JAVA_HOME
 echo "java version is"
